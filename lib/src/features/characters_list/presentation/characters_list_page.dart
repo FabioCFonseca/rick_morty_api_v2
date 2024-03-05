6b57fd49405a78c7b24a724628a1a9199702cad3
Fabio <fc_102@hotmail.com>
2024-03-04 22:16:37 -0300

    Presentation for character list feature

---
+++ lib/src/features/characters_list/presentation/characters_list_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:rick_morty_flutter/src/features/characters_list/presentation/ui/characters_success.dart';
+
+class CharactersListPage extends StatelessWidget {
+  const CharactersListPage({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CharactersSuccess();
+  }
+}
