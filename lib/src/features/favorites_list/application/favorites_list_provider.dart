d1483405c1a052a5a34dc9b37e7a9a20bd46806f
Fabio <fc_102@hotmail.com>
2024-03-05 10:07:36 -0300

    Added controllers

---
+++ lib/src/features/favorites_list/application/favorites_list_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:rick_morty_flutter/src/features/favorites_list/domain/favorites_model.dart';
+import 'package:rick_morty_flutter/src/utils/user_preferences.dart';
+
+class FavoritesListProvider extends ChangeNotifier {
+  List<FavoritesModel> favoriteCharacters = [];
+
+  //TOGGLE FUNCTIONALITY
+  void toggleFavorite(FavoritesModel character) {
+    final index = favoriteCharacters.indexOf(character);
+    if (index == -1) {
+      favoriteCharacters.add(character);
+    } else {
+      favoriteCharacters.removeAt(index);
+    }
+    notifyListeners();
+  }
+}
