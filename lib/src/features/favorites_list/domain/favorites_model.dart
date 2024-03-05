ee398d59d14c64e29765e6059c35aeeb690db86c
Fabio <fc_102@hotmail.com>
2024-03-04 21:51:22 -0300

    Added models

---
+++ lib/src/features/favorites_list/domain/favorites_model.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: hash_and_equals
+
+// Todos os domínios são iguais nas diferentes soluções de state management
+// tanto a interface do repositório quanto o model permitindo o re uso do código
+// devido ao desacoplamento
+
+class FavoritesModel {
+  final int id;
+  final String name;
+  final String image;
+
+  const FavoritesModel({
+    required this.id,
+    required this.name,
+    required this.image,
+  });
+
+  @override
+  String toString() => 'Character name: $name';
+
+  // Método para verificar a equalidade dos objetos para a funcionalidade
+  // de toggle nos favoritos
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    if (other is! FavoritesModel) return false;
+    return name == other.name && image == other.image;
+  }
+}
